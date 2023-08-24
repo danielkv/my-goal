1108ce063ca4adc066f9174ae805379baf35f1c5
Daniel K. Guolo <daniel_kv@hotmail.com>
2023-08-24 16:00:57 -0300

    [cli] fix movements migration

--- packages/cli/migrations/1692201872466-81dd4ebcae54.ts
+++ packages/cli/migrations/1692201872466-81dd4ebcae54.ts
@@ -5,38 +5,39 @@
 import * as admin from 'firebase-admin'
 
 const movements = [
-    'Front Squat',
-    'Back Squat',
-    'Overhead Squat',
-    'Deadlift',
-    'Sumo Deadlift High Pull',
-    'Push Press',
-    'Push Jerk',
-    'Split Jerk',
-    'Power Clean',
-    'Hang Clean',
-    'Snatch',
-    'Kettlebell Swing',
-    'Turkish Get-Up',
-    'Thruster',
-    'Overhead Lunge',
-    "Farmer's Carry",
-    'Sled Push/Pull',
-    'Clean and Jerk',
-    'Sandbag Clean',
-    'Dumbbell Snatch',
-    'Dumbbell Clean and Jerk',
-    'Atlas Stone Lift',
-    'Barbell Complex',
-    'Yoke Carry',
-    'Prowler Push/Pull',
-    'Log Lift',
-    'Axle Deadlift',
-    'Tire Flip',
-    'Sandbag Shouldering',
-    'Medicine Ball Clean',
-    'D-ball Over Shoulder',
-    'Axle Clean and Press',
+    { movement: 'Front Squat', resultType: 'weight', countResults: 0 },
+    { movement: 'Back Squat', resultType: 'weight', countResults: 0 },
+    { movement: 'Overhead Squat', resultType: 'weight', countResults: 0 },
+    { movement: 'Deadlift', resultType: 'weight', countResults: 0 },
+    { movement: 'Sumo Deadlift High Pull', resultType: 'weight', countResults: 0 },
+    { movement: 'Push Press', resultType: 'weight', countResults: 0 },
+    { movement: 'Push Jerk', resultType: 'weight', countResults: 0 },
+    { movement: 'Split Jerk', resultType: 'weight', countResults: 0 },
+    { movement: 'Power Clean', resultType: 'weight', countResults: 0 },
+    { movement: 'Hang Clean', resultType: 'weight', countResults: 0 },
+    { movement: 'Snatch', resultType: 'weight', countResults: 0 },
+    { movement: 'Kettlebell Swing', resultType: 'weight', countResults: 0 },
+    { movement: 'Turkish Get-Up', resultType: 'weight', countResults: 0 },
+    { movement: 'Thruster', resultType: 'weight', countResults: 0 },
+    { movement: 'Overhead Lunge', resultType: 'weight', countResults: 0 },
+    { movement: "Farmer's Carry", resultType: 'weight', countResults: 0 },
+    { movement: 'Sled Push/Pull', resultType: 'weight', countResults: 0 },
+    { movement: 'Clean and Jerk', resultType: 'weight', countResults: 0 },
+    { movement: 'Sandbag Clean', resultType: 'weight', countResults: 0 },
+    { movement: 'Dumbbell Snatch', resultType: 'weight', countResults: 0 },
+    { movement: 'Dumbbell Clean and Jerk', resultType: 'weight', countResults: 0 },
+    { movement: 'Atlas Stone Lift', resultType: 'weight', countResults: 0 },
+    { movement: 'Barbell Complex', resultType: 'weight', countResults: 0 },
+    { movement: 'Yoke Carry', resultType: 'weight', countResults: 0 },
+    { movement: 'Prowler Push/Pull', resultType: 'weight', countResults: 0 },
+    { movement: 'Log Lift', resultType: 'weight', countResults: 0 },
+    { movement: 'Axle Deadlift', resultType: 'weight', countResults: 0 },
+    { movement: 'Tire Flip', resultType: 'weight', countResults: 0 },
+    { movement: 'Sandbag Shouldering', resultType: 'weight', countResults: 0 },
+    { movement: 'Medicine Ball Clean', resultType: 'weight', countResults: 0 },
+    { movement: 'D-ball Over Shoulder', resultType: 'weight', countResults: 0 },
+    { movement: 'Axle Clean and Press', resultType: 'weight', countResults: 0 },
+    { movement: 'Max BMU 5min', resultType: 'reps', countResults: 0 },
 ]
 
 export async function migrate(db: admin.firestore.Firestore) {
@@ -45,7 +46,7 @@ export async function migrate(db: admin.firestore.Firestore) {
     await db.runTransaction(async (transaction) => {
         movements.forEach((movement) => {
             const docRef = movementsCollection.doc()
-            transaction.create(docRef, { movement })
+            transaction.create(docRef, movement)
         })
     })
 }
