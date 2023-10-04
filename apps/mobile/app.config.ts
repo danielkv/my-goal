7d65ef2a9cb17b680840afedb717f7a4719f9e8d
Daniel K. Guolo <daniel_kv@hotmail.com>
2023-10-04 14:39:14 -0300

    [mobile] fix rn-firebase version

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -13,7 +13,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         owner: 'goal',
         name: IS_PROD ? 'My Goal' : `My Goal (${ID_PREFIX})`,
         slug: 'goal-mobile',
-        version: '1.4.1',
+        version: '1.4.2',
         icon: './src/assets/icon.png',
         userInterfaceStyle: 'dark',
         scheme: 'mygoal',
