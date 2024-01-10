2d286dfa6b4c75668d7ba6a985b2915313aa363a
Daniel K. Guolo <daniel_kv@hotmail.com>
2024-01-10 18:15:37 -0300

    [mobile] remove date limit

--- apps/mobile/src/domain/useCases/worksheet/getWorksheetList.ts
+++ apps/mobile/src/domain/useCases/worksheet/getWorksheetList.ts
@@ -3,15 +3,12 @@ import { collections } from 'goal-utils'
 
 import { firebaseProvider } from '@common/providers/firebase'
 
-const LIMIT_DATE = '2024-01-08'
-
 export function getWorksheetListUseCase(): Promise<IWorksheetModel[]> {
     const fs = firebaseProvider.getFirestore()
 
     return fs
         .collection<IWorksheetModel>(collections.WORKSHEETS)
         .orderBy('startDate', 'desc')
-        .where('startDate', '<=', LIMIT_DATE)
         .where('published', '==', true)
         .get({ source: 'server' })
         .then((snapshot) => {
