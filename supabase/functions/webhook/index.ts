d1a69b8ca419df0a37cf8dae736bd57204b4777b
Daniel K. Guolo <daniel_kv@hotmail.com>
2024-03-04 21:07:53 -0300

    [supabase] fix process stripe event

--- supabase/functions/webhook/index.ts
+++ supabase/functions/webhook/index.ts
@@ -21,7 +21,7 @@ app.use(
 app.post('/webhook/stripe', express.raw({ type: 'application/json' }), async (req, res) => {
     try {
         const stripe = createStripe()
-        const endpointSecret = Deno.env.get('STRIPE_WEBHOOK_SIGNATURE') || ''
+        const endpointSecret = 'whsec_9a800a001cf17e194ae01f07f9aff7babaafcd7440177b2098dfee0f495c5df7' //Deno.env.get('STRIPE_WEBHOOK_SIGNATURE') || ''
         const sig = req.header('stripe-signature')
         if (!sig) throw new Error('Stripe signature is empty')
 
@@ -37,7 +37,6 @@ app.post('/webhook/stripe', express.raw({ type: 'application/json' }), async (re
 
         res.json({ received: true })
     } catch (err) {
-        console.log(err.message)
         res.status(400).send(err.message)
     }
 })
@@ -58,20 +57,37 @@ async function processProductsObject(session: Stripe.Checkout.Session) {
 
     const supabase = createSupabaseSuperClient()
 
-    const { error, data: user } = await supabase
+    // deno-lint-ignore no-explicit-any
+    let user: Record<string, any>
+    const userEmail = session.customer_details?.email
+    if (!userEmail) throw new Error('Invalid user email')
+
+    const { error, data: retrievedUser } = await supabase
         .from('users')
         .select()
         .eq('email', session.customer_details?.email)
-        .single()
+        .maybeSingle()
     if (error) throw error
+
+    if (!retrievedUser) {
+        const { error, data: newUser } = await supabase.auth.admin.createUser({
+            email: userEmail,
+            phone: session.customer_details?.phone || undefined,
+            user_metadata: { displayName: session.customer_details?.name },
+        })
+        if (error) throw error
+        user = newUser
+    } else user = retrievedUser
 
     const promises = items.map(
         (item) =>
             // deno-lint-ignore no-async-promise-executor
-            new Promise(async (resolve) => {
+            new Promise(async (resolve, reject) => {
                 const productId = item.price?.product
-                if (!productId || typeof productId !== 'string') throw new Error('Produto não está listado')
+                if (!productId || typeof productId !== 'string') return reject('Produto não está listado')
                 const product = await stripe.products.retrieve(productId)
+
+                if (!product?.metadata?.category) return reject('Product category is empty')
 
                 switch (product.metadata.category) {
                     case 'program': {
@@ -80,10 +96,12 @@ async function processProductsObject(session: Stripe.Checkout.Session) {
                     }
                     case 'worksheet': {
                         const token = session.subscription
-                        if (!token || !session.customer) throw new Error('Data is incomplete')
+                        if (!token || !session.customer) return reject('Data is incomplete')
                         await processWorksheetPayment(supabase, user.email, token as string, session.customer as string)
                         break
                     }
+                    default:
+                        return reject('This Category cannot be handled')
                 }
 
                 return resolve(true)
