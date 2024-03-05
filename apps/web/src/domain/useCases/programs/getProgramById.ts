78d243aacdef4c6312767919cf82cf6dc7e3a843
Daniel K. Guolo <daniel_kv@hotmail.com>
2024-03-04 21:52:49 -0300

    [web] program order

--- apps/web/src/domain/useCases/programs/getProgramById.ts
+++ apps/web/src/domain/useCases/programs/getProgramById.ts
@@ -8,6 +8,10 @@ export async function getProgramByIdUseCase(id: string): Promise<IProgram> {
         .select(
             '*, segments:program_segments(*, sessions:program_sessions(*, groups:program_groups(*, movements:program_movements(*))))'
         )
+        .order('order', { referencedTable: 'program_segments' })
+        .order('order', { referencedTable: 'program_sessions' })
+        .order('order', { referencedTable: 'program_groups' })
+        .order('order', { referencedTable: 'program_movements' })
         .eq('id', id)
         .single()
 
