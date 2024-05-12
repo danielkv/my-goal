a4f6034551ec546efde56629b517e092ac59ed24
Daniel K. Guolo <daniel_kv@hotmail.com>
2024-05-12 15:18:02 -0300

    [web] add payment link in program card

--- apps/web/src/common/components/ProgramItem/index.tsx
+++ apps/web/src/common/components/ProgramItem/index.tsx
@@ -1,10 +1,11 @@
 import { Models } from 'goal-models'
 import { pluralize } from 'goal-utils'
+import { FiLink } from 'solid-icons/fi'
 
 import { Component } from 'solid-js'
 
 import { useNavigate } from '@solidjs/router'
-import { Card, Stack } from '@suid/material'
+import { Card, IconButton, Stack } from '@suid/material'
 
 interface ProgramItemProps {
     program: Models<'programs'>
@@ -16,11 +17,24 @@ const ProgramItem: Component<ProgramItemProps> = (props) => {
 
     return (
         <Card
-            class="cursor-pointer hover:bg-gray-800"
+            class="cursor-pointer hover:bg-gray-800 relative"
             onclick={() => navigate(`/dashboard/program/${props.program.id}`)}
         >
             <Stack direction="row">
                 <img src={props.program.image} class="w-[100px] h-auto object-cover" />
+                {props.program.payment_link_url && (
+                    <Stack position="absolute" right={10} top={10}>
+                        <IconButton
+                            size="small"
+                            onClick={(e) => {
+                                e.stopPropagation()
+                                window.open(props.program.payment_link_url!)
+                            }}
+                        >
+                            <FiLink />
+                        </IconButton>
+                    </Stack>
+                )}
                 <Stack px={2} py={1}>
                     <h3 class="text-lg font-bold">{props.program.name}</h3>
                     <div class="text-sm text-gray-400">Valor atual: R$ {props.program.amount.toFixed(2)}</div>
