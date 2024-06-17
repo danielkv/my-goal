7d5b1c931173611426270ed2822fac6279249d22
Daniel K. Guolo <daniel_kv@hotmail.com>
2024-06-17 10:09:42 -0300

    [web] proxy api calls

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,19 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import solidSvg from 'vite-plugin-solid-svg'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import suidPlugin from '@suid/vite-plugin'
 
-export default defineConfig({
-    plugins: [suidPlugin(), solidPlugin(), solidSvg(), tsconfigPaths()],
-    server: {
-        port: 3001,
-    },
-    build: {
-        target: 'esnext',
-    },
-    optimizeDeps: {
-        include: ['prosemirror-state', 'prosemirror-transform', 'prosemirror-model', 'prosemirror-view'],
-    },
+export default defineConfig(({ mode }) => {
+    const envs = loadEnv(mode, './')
+    process.env = { ...process.env, ...envs }
+    return {
+        plugins: [suidPlugin(), solidPlugin(), solidSvg(), tsconfigPaths()],
+        server: {
+            port: 3001,
+            proxy: {
+                '/api': {
+                    target: process.env.VITE_SUPABASE_FUNCTION_BASE_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                    configure(proxy) {
+                        proxy.on('proxyReq', (req) => {
+                            req.setHeader('user-agent', 'Custom-User-Agent/1.0')
+                        })
+                    },
+                },
+            },
+        },
+        build: {
+            target: 'esnext',
+        },
+        optimizeDeps: {
+            include: ['prosemirror-state', 'prosemirror-transform', 'prosemirror-model', 'prosemirror-view'],
+        },
+    }
 })
